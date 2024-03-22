5f9c2df93303776bc95f577e212a935bf9521dcb
Nitin Porwal <nitinporwal@microsoft.com>
2024-03-22 21:41:10 +0530

    add tailwind styling and add navbar with routing pages

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from './Home'; // Import the 'Home' component
+import About from './About'; // Import the 'Home' component
+import Projects from './Projects'; // Import the 'Home' component
+import Contact from './Contact'; // Import the 'Home' component
+
+export {
+  Home,
+  About,
+  Projects,
+  Contact}
