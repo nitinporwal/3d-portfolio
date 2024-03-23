bf770838a8821bf8302870687ce71363ae6caa20
Nitin Porwal <nitinporwal@microsoft.com>
2024-03-23 14:31:41 +0530

    add lighting to island

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
 import Loader from '../components/Loader'
@@ -23,11 +24,9 @@ const Home = () => {
       <Canvas className='w-full h-screen bg-transparent'
       camera={{near: 0.1, far: 1000}}>
         <Suspense fallback={<Loader/>}>
-          <directionalLight/>
-          <ambientLight/>
-          <pointLight/>
-          <spotLight/>
-          <hemisphereLight/>
+          <directionalLight position={[1, 1, 1]} intensity={2}/>
+          <ambientLight intensity={0.5}/>
+          <hemisphereLight skyColor = "#b1e1ff" groundColor="#000000" intensity={1}/>
 
           <Island position={islandPosition} scale = {islandScale} rotation = {islandRotation}/>
         </Suspense>
