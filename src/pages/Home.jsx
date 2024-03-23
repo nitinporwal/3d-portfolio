82142fe9d78873005f090f5df7bf8a71de45ea45
Nitin Porwal <nitinporwal@microsoft.com>
2024-03-23 12:43:06 +0530

    add loader and island model in home page and add assets folder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
+import Loader from '../components/Loader'
+import Island from '../models/Island'
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0]
+
+    if(window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else { 
+      screenScale = [1, 1, 1];
+    }
+    return [ screenScale, screenPosition, rotation ];
+  }
+
+  const [ islandScale, islandPosition, islandRotation ] = adjustIslandForScreenSize();
   return (
-    <div>Home</div>
+    <section className='w-full h-screen relative'>
+      <Canvas className='w-full h-screen bg-transparent'
+      camera={{near: 0.1, far: 1000}}>
+        <Suspense fallback={<Loader/>}>
+          <directionalLight/>
+          <ambientLight/>
+          <pointLight/>
+          <spotLight/>
+          <hemisphereLight/>
+
+          <Island position={islandPosition} scale = {islandScale} rotation = {islandRotation}/>
+        </Suspense>
+      </Canvas>
+    </section>
   )
 }
 
